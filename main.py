93f08c423155e73b6a3d9f69c34ada583c926ec0
Drea Loeper <90044489+DreaLo@users.noreply.github.com>
2021-11-11 19:49:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ def index():
     secret_number = request.cookies.get("secret_number")  # check if there is already a cookie named secret_number
 
     response = make_response(render_template("index.html"))
-    if not secret_number:  # if not, create a new cookie
-        new_secret = random.randint(1, 30)
-        response.set_cookie("secret_number", str(new_secret))
+    #if not secret_number:  # if not, create a new cookie
+      new_secret = random.randint(1, 30)
+      response.set_cookie("secret_number", str(new_secret))
 
     return response
 
